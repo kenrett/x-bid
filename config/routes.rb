c5b25cefef22b95edb41d780dfdab3d537638d7d
Ken Rettberg <kenrettberg@gmail.com>
2025-09-06 19:02:42 -0700

    Adds user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
   get "up" => "rails/health#show", :as => :rails_health_check
+  
+  resource :session
+  resource :registration, only: %i[new create]
+  resources :passwords, param: :token
 
   root "home#show"
 end
